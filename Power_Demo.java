d159a29e063396dbebf568c818d83664a19a96df
SubramaniMani <142685216+SubramaniMani@users.noreply.github.com>
2023-08-22 08:24:46 +0530

    program4

---
+++ Power_Demo.java
@@ -0,0 +1,101 @@
+package loops;
+
+public class Power_Demo {
+
+	public static void main(String[] args) {
+		
+		
+		Power_Demo pd = new Power_Demo();
+		
+		/*int no = 1;
+		while(no<=5)
+		{
+			pd.find_power(no,no);	
+			
+			no = no+1;
+		}
+	
+		
+	}
+
+	private void find_power(int base, int power) {
+		
+		int no = 1;
+		while(power>0)
+		{
+			no=no*base;
+			power=power-1;
+		}
+		System.out.println(no);
+	}*/
+		
+		
+		
+		/*int no = 1;
+		while(no<=5)
+		{
+			pd.find_power(no,no-1);	
+			
+			no = no+1;
+		}
+	
+		
+	}
+
+	private void find_power(int base, int power) {
+		
+		int no = 1;
+		while(power>0)
+		{
+			no=no*base;
+			power=power-1;
+		}
+		System.out.println(no);
+	}*/
+
+		/*int no = 1;
+		while(no<=5)
+		{
+			pd.find_power(no,no+2);	
+			
+			no = no+1;
+		}
+	
+		
+	}
+
+	private void find_power(int base, int power) {
+		
+		int no = 1;
+		while(power>0)
+		{
+			no=no*base;
+			power=power-1;
+		}
+		System.out.println(no);
+	}*/
+
+		int no = 1;
+		while(no<=5)
+		{
+			pd.find_power(no,no+3);	
+			
+			no = no+1;
+		}
+	
+		
+	}
+
+	private void find_power(int base, int power) {
+		
+		int no = 1;
+		while(power>0)
+		{
+			no=no*base;
+			power=power-1;
+		}
+		System.out.println(no);
+	}
+
+
+}
