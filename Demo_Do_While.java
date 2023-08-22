1081023767cad02515810c4050dac3f4bd7dba62
SubramaniMani <142685216+SubramaniMani@users.noreply.github.com>
2023-08-22 08:20:37 +0530

    Demo_while_progarm

---
+++ Demo_Do_While.java
@@ -0,0 +1,20 @@
+package While_Loop;
+
+public class Demo_Do_While {
+
+	public static void main(String[] args) {
+		
+		
+		int number = 2;
+		do
+		{
+			System.out.println(number);
+			number++;
+		}
+		while(number>10);
+	}
+	
+	
+	
+
+}
